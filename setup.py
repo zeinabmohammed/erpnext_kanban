f197662089c84d0073f6166d01f1a57d1b4bd400
Zynab <43417377+zeinabmohammed@users.noreply.github.com>
2021-08-08 20:59:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+import re
+from ast import literal_eval
 
 version = '0.0.1'
-requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as r:
+	install_requires = r.read().strip().split('\n')
 
 setup(
 	name='kanban',
@@ -14,6 +16,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	include_package_data=True,
+	install_requires=install_requires
 )
